d8eb13d802564c908898fe985a76b6199c255e09
micastel <micastel@cisco.com>
2017-03-13 12:10:48 -0400

    Pulls new user and prints to PDF

--- ISE_API_Testet.py
+++ ISE_API_Testet.py
@@ -1,23 +1,139 @@
-import requests
-import base64
-from requests.auth import HTTPBasicAuth
-from basicauth import encode
+import requests, time
+from string import letters, digits, printable
+import datetime
+from lxml import etree
 
-uname,passwrd='ers-admin', 'Csap17'
+user = 'sponsor'
+pwd  = 'Csap1'
+ip="198.18.133.27"
 
-encode_str=encode(uname, passwrd)
-print encode_str.replace('Basic ','')
-print 'ZXJzLWFkbWluOkNzYXAxNw=='
+
+def chron_job():
+	while(1):
+		tme = datetime.datetime.now()
+		if str(tme)[17:19] == '59' and (str(tme)[20:23]) >= '999':
+			print "hit: "+str(tme)
+			time.sleep(.2)
+
+def get_convert_time(datetime):
+	abbrv = {'01':'jan', '02':'feb', '03':'mar', '04':'apr', '05':'may', '06':'jun', '07':'jul', '08':'aug', '09':'sep', '10':'oct', '11':'nov', '12':'dec'}
+	dash = '-'
+	dot = '.'
+	space = ' '
+	
+	time_s = str(datetime)
+	year = time_s[2:4]
+	month = time_s[5:7]
+	day = time_s[8:10]
+	if int(time_s[11:13])%12 < 10:
+		hh = '0'+str(int(time_s[11:13])%12)
+	else:
+		hh = str(int(time_s[11:13])%12)
+	mm = time_s[14:16]
+	ss = time_s[17:19]
+	new_time= day+dash+abbrv[month]+dash+year+space+hh+dot+mm+dot+ss
+
+	return new_time
+
 
 #Disable warnings???
 requests.packages.urllib3.disable_warnings()
+def recent_guests(user, pwd, ip):
+	id_list=[]
+	url = "https://"+ip+":9060/ers/config/guestuser?filter=creationTime.STARTSW.10-mar-17"#+off_myself
+	headers={
+		'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
+		'Content-Type': 'application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
+	}
+	print url
+	response = requests.request("GET", url, auth=(user,pwd), headers=headers, verify=False)
+	root = etree.fromstring(str(response.text))
+	if int(root.attrib['total'])!= 0:
+		print "Response:"
+		print etree.tostring(root, pretty_print=True)
+		print '\n'
+	print
+	print "Number of recent : "+ str(root.attrib['total'])
+	for count in range (0, int(root.attrib['total'])):
+		print str(count+1)+".\t"+"Guest: "+str(root[0][0].attrib['name'])
+		print"\t"+str(root[0][0].tag)+" : "+str(root[0][0].attrib['id'])
+		id_list.append(str(root[0][0].attrib['id']))
+	return id_list
 
-ip="198.18.133.27:9060"
-url = "https://"+ip+"/ers/config/guestuser/"
-headers={
-	'Authorization': encode_str,
-	'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
-	#'content-type': 'application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
-}
-response = requests.request("GET", url, headers=headers, verify=False)
-print response
+def guest_user_by_id(user, pwd, ip, id_list):
+	for resource in id_list:
+		url = "https://"+ip+":9060/ers/config/guestuser/"+resource
+		headers={
+			'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
+			'Content-Type': 'application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
+		}
+		response = requests.request("GET", url, auth=(user,pwd), headers=headers, verify=False)
+		root = etree.fromstring(str(response.text))
+		print "\n\nResponse:\n"
+		print etree.tostring(root, pretty_print=True)
+		print
+		print (root[3][4].tag, root[3][4].text)
+		print (root[3][5].tag, root[3][5].text)
+		print (root[3][0].tag, root[3][0].text)
+		print '*************'
+		print (root[3][1].tag, root[3][1].text)
+		print '*************'
+
+
+		# for child in root:
+		# 	for grand in child:
+		# 		print(grand.tag, grand.text)
+		#print
+
+		file = open('ise-out.txt', 'w')
+		file.write(''+str(root[3][4].tag)+' : '+str(root[3][4].text)+'\n')
+		file.write(''+str(root[3][5].tag)+' : '+str(root[3][5].text)+'\n\n')
+		file.write(''+str(root[3][0].tag)+' : '+str(root[3][0].text)+'\n\n')
+		file.write(''+str(root[5].tag)+' : '+str(root[5].text)+'\n')
+		file.close()
+
+
+		
+
+def all_guest_users(user, pwd, ip):
+	url = "https://"+ip+":9060/ers/config/guestuser/785f88a0-0449-11e7-a43a-005056a918d5"
+	headers={
+		'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
+		'Content-Type': 'application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
+	}
+	response = requests.request("GET", url, auth=(user,pwd), headers=headers, verify=False)
+	root = etree.fromstring(str(response.text))
+	print "\n\nResponse:\n"
+	print etree.tostring(root, pretty_print=True)
+
+
+def new_guest(user, pwd, ip):
+	url = "https://"+ip+":9060/ers/config/guestuser"
+	headers={
+		'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
+		'Content-Type': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
+	}
+
+	body=''
+	file = open('post-ise.xml', 'r')
+	for line in file.readlines():
+		body+=str(line)
+	file.close
+	response = requests.request("POST", url, auth=(user,pwd), headers=headers, data=body, verify=False)
+
+
+
+if __name__ == '__main__':
+	print '\n\n\n========================================'
+	print '|+++\t\t\t            +++|\n|+++      ISE SOFTWARE PROJECT      +++|'
+	print '|+++\t\t\t            +++|'
+	print '========================================\n\n'
+
+
+	
+	#chron_job()
+	res_list = recent_guests(user, pwd, ip)
+	guest_user_by_id(user, pwd, ip, res_list)
+	#all_guest_users(user, pwd, ip)
+	#new_guest(user, pwd, ip)
+
--- printer_test.py
+++ printer_test.py
@@ -3,7 +3,7 @@ from reportlab.pdfgen import canvas
 from subprocess import call
 import time, platform, sys#, win32api
 
-filename = raw_input("file to print: ")#"printer_output.pdf"
+filename = 'printer_output.pdf'#raw_input("file to print: ")#"printer_output.pdf"
 os_plat = platform.system()
 
 """
@@ -13,27 +13,46 @@ pdf.drawString(72, 648, "@Mike Castellana")
 pdf.drawString(72, 576, "Printing From Script Test")
 pdf.save()
 """
+pdf = canvas.Canvas("printer_output.pdf", pagesize=letter)
+pdf.drawString(72, 720, time.ctime(time.time()))
+y=648 #36
+file = open('ise-out.txt', 'r')
+for line in file.readlines():
+	if line != '\n':
+		line = str(line).replace('\n','')
+		pdf.drawString(72, y, str(line))
+	#print line
+	y-=36
+file.close
+pdf.save()
+#sys.exit(0)
 
 printer_ip = "64.102.40.215"#raw_input("Printer IP: ")
 
 if os_plat.lower() == 'darwin' or os_plat.lower() == 'linux':
 	if os_plat.lower() == 'darwin': print "Running on Mac OS X"
 	elif os_plat.lower() == 'linux':print "Running on MAC OS X" 
-	# IPP_addr = "https://"+printer_ip
-	# printer_name = "ISE_SCRIPT_PRINTER"
-	# print_name_arg = "-P"+printer_name
+	IPP_addr = "https://"+printer_ip
+	printer_name = "ISE_SCRIPT_PRINTER"
+	print_name_arg = "-P"+printer_name
 	
-	# call(["lpadmin", "-E", "-p", printer_name, "-v", IPP_addr, "-E"])
-	# call(["lpr", print_name_arg, filename])
-	# call(["lpadmin", "-x", printer_name])
+	#print "\nlpadmin", "-E", "-p", printer_name, "-v", IPP_addr, "-E"
+	#print "lpr", print_name_arg, filename
+	call(["lpadmin", "-E", "-p", printer_name, "-v", IPP_addr, "-E"])
+	print "Initializing Printer..."
+	time.sleep(2)
+	call(["lpr", print_name_arg, filename])
+	print "Printing..."
+	time.sleep(2)
+	call(["lpadmin", "-x", printer_name])
+	print "Tearing down connection..."
+	time.sleep(2)
 
 elif os_plat.lower() == 'windows':
 	print "Running on Windows"
 	print_name_arg = "-S"+printer_name
 	win32api.ShellExecute (["print https://64.102.40.215 filename"])
 	win32api.ShellExecute (["lpr", print_name_arg, "-d", filename])
-
-
 
 else:
 	print "Incompatible OS -- application designed to work with Mac, Windows, or Linux based Kernels"
