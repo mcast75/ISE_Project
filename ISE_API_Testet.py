c875cfc2c9206db2a49de7e745fb8a99ecde3ebf
micastel <micastel@cisco.com>
2017-03-13 12:34:26 -0400

    Fixed time bug

--- ISE_API_Testet.py
+++ ISE_API_Testet.py
@@ -11,8 +11,13 @@ ip="198.18.133.27"
 def chron_job():
 	while(1):
 		tme = datetime.datetime.now()
-		if str(tme)[17:19] == '59' and (str(tme)[20:23]) >= '999':
-			print "hit: "+str(tme)
+		# if str(tme)[17:19] == '59' and (str(tme)[20:23]) >= '999':
+		# 	print "hit: "+str(tme)
+		# 	time.sleep(.2)
+		if str(tme)[18] == '9' and (str(tme)[20:23]) >= '999':
+			print "hit [datetime]:\t"+str(tme)
+			print "hit [convtime]:\t"+str(get_convert_time(tme))
+			print
 			time.sleep(.2)
 
 def get_convert_time(datetime):
@@ -25,9 +30,15 @@ def get_convert_time(datetime):
 	year = time_s[2:4]
 	month = time_s[5:7]
 	day = time_s[8:10]
-	if int(time_s[11:13])%12 < 10:
+	print int(time_s[11:13]) == 12
+	if int(time_s[11:13]) == 12:
+		print '2'
+		hh = str(int(time_s[11:13]))
+	elif int(time_s[11:13])%12 < 10:
+		print '1'
 		hh = '0'+str(int(time_s[11:13])%12)
 	else:
+		print '3'
 		hh = str(int(time_s[11:13])%12)
 	mm = time_s[14:16]
 	ss = time_s[17:19]
@@ -40,7 +51,7 @@ def get_convert_time(datetime):
 requests.packages.urllib3.disable_warnings()
 def recent_guests(user, pwd, ip):
 	id_list=[]
-	url = "https://"+ip+":9060/ers/config/guestuser?filter=creationTime.STARTSW.10-mar-17"#+off_myself
+	url = "https://"+ip+":9060/ers/config/guestuser?filter=creationTime.STARTSW.10-mar-17"
 	headers={
 		'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
 		'Content-Type': 'application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
@@ -96,7 +107,7 @@ def guest_user_by_id(user, pwd, ip, id_list):
 		
 
 def all_guest_users(user, pwd, ip):
-	url = "https://"+ip+":9060/ers/config/guestuser/785f88a0-0449-11e7-a43a-005056a918d5"
+	url = "https://"+ip+":9060/ers/config/guestuser/"
 	headers={
 		'Accept': "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
 		'Content-Type': 'application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
@@ -131,9 +142,9 @@ if __name__ == '__main__':
 
 
 	
-	#chron_job()
-	res_list = recent_guests(user, pwd, ip)
-	guest_user_by_id(user, pwd, ip, res_list)
+	chron_job()
+	#res_list = recent_guests(user, pwd, ip)
+	#guest_user_by_id(user, pwd, ip, res_list)
 	#all_guest_users(user, pwd, ip)
 	#new_guest(user, pwd, ip)
 
